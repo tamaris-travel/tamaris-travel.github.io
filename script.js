18b04b68664e6d915de9e2f81298a75cc1430f2f
riswan29 <wandra290401@gmail.com>
2022-10-09 13:56:23 +0700

    done

--- script.js
+++ script.js
@@ -70,4 +70,40 @@ $(document).ready(function(){
             }
         }
     });
+    $(document).on('click','.send', function(){
+        /* Inputan Formulir */
+        var input_name          = $("#name").val(),
+            input_tujuan        = $("#tujuan").val(),
+            input_alamat        = $("#alamat").val(),
+            input_product       = $("#product").val(),
+            input_description   = $("#description").val();
+     
+        /* Pengaturan Whatsapp */
+        var walink      = 'https://web.whatsapp.com/send',
+            phone       = '6283147372179',
+            text        = 'Halo saya ingin memesan ',
+            text_yes    = 'Pesanan Anda berhasil terkirim.',
+            text_no     = 'Isilah formulir terlebih dahulu.';
+     
+        /* Smartphone Support */
+        if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+            var walink = 'whatsapp://send';
+        }
+     
+        if(input_name != "" && input_alamat != "" && input_product != ""){
+            /* Whatsapp URL */
+            var checkout_whatsapp = walink + '?phone=' + phone + '&text=' + text + '%0A%0A' +
+                '*Nama* : ' + input_name + '%0A' +
+                '*Alamat* : ' + input_alamat+ '%0A' +
+                '*Tujuan : ' + input_tujuan + '%0A' +
+                '*Produk* : ' + input_product + '%0A' +
+                '*Catatan* : ' + input_description + '%0A';
+     
+            /* Whatsapp Window Open */
+            window.open(checkout_whatsapp,'_blank');
+            document.getElementById("text-info").innerHTML = '<div class="alert alert-success">'+text_yes+'</div>';
+        } else {
+            document.getElementById("text-info").innerHTML = '<div class="alert alert-danger">'+text_no+'</div>';
+        }
+    });
 });
