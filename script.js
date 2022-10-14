0ef081c370a8725735a3f04c682239dad6a19214
Enjelta <angeltauruk@gmail.com>
2022-10-14 14:34:40 -0700

    update

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Soekarno Hatta", "Halim Perdanakusuma", "Kerta Jati",],
+        strings: ["Soekarno Hatta", "Bandung", "Bekasi", "Halim Perdanakusuma", "Kerta Jati",],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["**Soekarno Hatta", "**Halim Perdanakusuma", "**Kerta Jati",],
+        strings: ["**Soekarno Hatta","**Bandung", "**Bekasi", "**Halim Perdanakusuma", "**Kerta Jati",],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
